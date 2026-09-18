6fe50df3ff0c2cfb6c614ee9a6a82dd76fba6e68
gayatrim-spektra <139850907+gayatrim-spektra@users.noreply.github.com>
2026-09-18 10:54:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+
+def run_command(user_input):
+    subprocess.call(user_input, shell=True)
+
+def get_password():
+    password = "admin123"
+    return password
+
+def read_file(filename):
+    with open("/var/www/" + filename) as f:
+        return f.read()
+
+run_command("ls")
+print(get_password())
